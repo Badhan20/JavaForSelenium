72d229742eb00fb54ed67bf9b361c480105b1249
BADHAN <BADHAN@BADHAN-PC>
2021-11-15 19:57:33 +0600

    Merged

---
+++ JavaForSelenium/src/p_05_LocalAndInstanceVariable/C_01_LocalVariable.java
@@ -0,0 +1,31 @@
+package p_05_LocalAndInstanceVariable;
+
+public class C_01_LocalVariable {
+
+	public static void main(String[] args) {
+	
+		//Block 
+		int x=10;
+		System.out.println("The value of x: "+x);
+		
+		C_01_LocalVariable obj = new C_01_LocalVariable();
+		
+		obj.marks();
+		obj.printNames();
+
+	}
+	
+	//Method
+	public void marks() {
+		int marks=90;
+		System.out.println("The marks is: "+marks);
+	}
+	
+	public void printNames() {
+		//Block
+		for (int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
